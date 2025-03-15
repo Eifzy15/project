e56310351666efec559381228c9838d84c00b8da
ify okpaleke <ifzy1915@gmail.com>
2025-03-15 14:11:44 +0100

    me commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-let bestPlayer = "Salah";
+let bestPlayer = "sAlaH";
 const PI = 3.142;
-console.log(bestPlayer);
+//console.log('good morning '+ "ydfhdg");// concatenation
+// console.log(bestPlayer[bestPlayer.length -1]);
+// console.log('A'.concat('h', 'dg', 'gd'));
+let address = 'Abakpa     ';
+address.
+console.log(bestPlayer.endsWith('H'));
+
+console.log(bestPlayer[0].toUpperCase() + bestPlayer.slice(1).toLowerCase());
+console.log('h'.toLowerCase() == 'H'.toLowerCase());
+
+
+bestPlayer[0]
