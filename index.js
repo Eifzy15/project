38bc942a60d81f1ac50e9056a7634537520e6789
ify okpaleke <ifzy1915@gmail.com>
2025-03-21 14:26:55 +0100

    me

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PI = 3.142;
 // console.log(bestPlayer[bestPlayer.length -1]);
 // console.log('A'.concat('h', 'dg', 'gd'));
 let address = 'Abakpa     ';
-address.
+address.at(8) //tyuutyyuuuuuu
 console.log(bestPlayer.endsWith('H'));
 
 console.log(bestPlayer[0].toUpperCase() + bestPlayer.slice(1).toLowerCase());
@@ -12,3 +12,8 @@ console.log('h'.toLowerCase() == 'H'.toLowerCase());
 
 
 bestPlayer[0]
+let bestfood = 'yamandoil'
+bestfood.at(9)
+console.log(bestfood.charAt)
+
+
