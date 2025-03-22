ffb607be57a95f361555bda08fad1674daa69ed7
ify okpaleke <ifzy1915@gmail.com>
2025-03-22 13:39:32 +0100

    fixed

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-let bestPlayer = "sAlaH";
-const PI = 3.142;
-//console.log('good morning '+ "ydfhdg");// concatenation
-// console.log(bestPlayer[bestPlayer.length -1]);
-// console.log('A'.concat('h', 'dg', 'gd'));
-let address = 'Abakpa     ';
-address.at(8) //tyuutyyuuuuuu
-console.log(bestPlayer.endsWith('H'));
+// let bestPlayer = "sAlaH";
+// const PI = 3.142;
+// //console.log('good morning '+ "ydfhdg");// concatenation
+// // console.log(bestPlayer[bestPlayer.length -1]);
+// // console.log('A'.concat('h', 'dg', 'gd'));
+// let address = 'Abakpa     ';
+// address.at(8) //tyuutyyuuuuuu
+// console.log(bestPlayer.endsWith('H'));
 
-console.log(bestPlayer[0].toUpperCase() + bestPlayer.slice(1).toLowerCase());
-console.log('h'.toLowerCase() == 'H'.toLowerCase());
+// console.log(bestPlayer[0].toUpperCase() + bestPlayer.slice(1).toLowerCase());
+// console.log('h'.toLowerCase() == 'H'.toLowerCase());
 
 
-bestPlayer[0]
-let bestfood = 'yamandoil'
-bestfood.at(9)
-console.log(bestfood.charAt)
+// bestPlayer[0]
+// let bestfood = 'yamandoil'
+// bestfood.at(9)
+// console.log(bestfood.at)
+
+let bestsport = "football"
+console.log(bestsport.split('t'));
+console.log(bestsport.toLowerCase());
+console.log(bestsport.toUpperCase());
+
+console.log(bestsport.slice(2, -2));
+console.log(bestsport.substring(5));
+console.log(bestsport.indexOf('t'));
+console.log(bestsport.lastIndexOf('l'));
+console.log(bestsport.includes('r'));
+console.log(bestsport.startsWith('t'));
+console.log(bestsport.endsWith('a'));
+console.log(bestsport.replace('o','a'));
+console.log(bestsport.replaceAll('o', 'u'));
+
+let firstname = 'ify'
+let lastname = 'okpaleke'
+console.log(firstname + " " + lastname);
+console.log(`${firstname} ${lastname}`);
+console.log(firstname.concat(" ",lastname));
+
+
+
+
+
 
 
